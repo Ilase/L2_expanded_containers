59721fcaa94e938f9b159608f45417212f1814a2
Ilase <ilya280505@gmail.com>
2024-03-03 13:21:51 +0300

    gradient

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      title: 'Expanded Containers',
       theme: ThemeData(
         backgroundColor: Colors.transparent
       ),
